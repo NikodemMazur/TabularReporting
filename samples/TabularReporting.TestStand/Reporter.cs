91b0f0c0e44403cdb7b47d0b1ce5878ddcd2c7cf
Anonymous <>
2020-03-11 09:46:05 +0100

    Modify inheritance. No longers inherits from TabularReporting.Reporter<>

--- samples/TabularReporting.TestStand/Reporter.cs
+++ samples/TabularReporting.TestStand/Reporter.cs
@@ -1,14 +1,19 @@
 ﻿using TabularReporting.Abstractions;
 using NationalInstruments.TestStand.Interop.API;
+using System.Collections.Generic;
 
 namespace TabularReporting.TestStand
 {
-    public class Reporter : Reporter<EnumerablePropertyObject>
+    public class Reporter
     {
+        Reporter<EnumerablePropertyObject> _reporter;
+
         public Reporter(PropertyObject source, params IRowQuery[] reportRowQueries) : base()
         {
-            Source = new EnumerablePropertyObject(source);
-            ReportQuery = new ColumnQueryWithRows(reportRowQueries);
+            _reporter = new Reporter<EnumerablePropertyObject>();
         }
+
+        public IColumn Report(PropertyObject source, IEnumerable<IRowQuery> reportRowQueries) =>
+            _reporter.Report(new EnumerablePropertyObject(source), new ColumnQueryWithRows(reportRowQueries));
     }
 }
