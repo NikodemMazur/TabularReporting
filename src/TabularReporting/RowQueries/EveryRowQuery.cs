89a595c8005109615feac7d2928f2dffd53bcc64
Anonymous <>
2020-03-13 19:56:21 +0100

    Add row query which always executes in projection

---
+++ src/TabularReporting/RowQueries/EveryRowQuery.cs
@@ -0,0 +1,22 @@
+﻿using TabularReporting.Abstractions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TabularReporting
+{
+    public class EveryRowQuery : IRowQuery
+    {
+        readonly IEnumerable<IColumnQuery> _colQueries;
+
+        public EveryRowQuery(IEnumerable<IColumnQuery> colQueries)
+        {
+            _colQueries = colQueries;
+        }
+        public EveryRowQuery() : this(Enumerable.Empty<IColumnQuery>()) { }
+        public EveryRowQuery(params IColumnQuery[] colQueries) : this(colQueries.AsEnumerable()) { }
+
+        public bool Predicate => true;
+
+        public IEnumerable<IColumnQuery> ColumnQueries => _colQueries;
+    }
+}
