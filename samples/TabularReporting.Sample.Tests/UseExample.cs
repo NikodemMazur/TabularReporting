2c2d555cab0441e9d9c0731dd28a37091692bdc1
Anonymous <>
2020-03-13 20:54:43 +0100

    Add 7 steps for demo

--- samples/TabularReporting.Sample.Tests/UseExample.cs
+++ samples/TabularReporting.Sample.Tests/UseExample.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using TabularReporting.Abstractions;
+using System.IO;
 
 namespace TabularReporting.Sample.Tests
 {
@@ -15,17 +16,17 @@ namespace TabularReporting.Sample.Tests
         {
             // 1. Prepare result
             TestResult result =
-                new TestResult("TestOfThings", TimeSpan.FromSeconds(10), true, null,
+                new TestResult("TestOfThings", TimeSpan.FromSeconds(10), false, null,
                     new TestResult("TestMethod0", TimeSpan.FromSeconds(2), true, "Equal", null),
-                    new TestResult("TestMethod1", TimeSpan.FromSeconds(2), true, "MultipleTrue",
-                        new TestResult("Subtest0", TimeSpan.FromSeconds(1), true, "True", null,
-                        new TestResult("Subtest1", TimeSpan.FromSeconds(1), true, "True", null))),
+                    new TestResult("TestMethod1", TimeSpan.FromSeconds(2), false, "MultiTrue",
+                        new TestResult("Subtest0", TimeSpan.FromSeconds(1), true, "True", null),
+                        new TestResult("Subtest1", TimeSpan.FromSeconds(1), false, "True", null)),
                     new TestResult("TestMethod2", TimeSpan.FromSeconds(2), true, "Contains", null),
                     new TestResult("TestMethod3", TimeSpan.FromSeconds(2), true, "Equal", null),
                     new TestResult("TestMethod4", TimeSpan.FromSeconds(2), true, "Empty", null));
 
             // 2. Define column (report) query
-            
+            IColumnQuery counterColQuery;
             IColumnQuery reportQuery =
                 new ColumnQueryWithRows(
                     new OneTimeRowQuery( // 2a. Define first header row
@@ -34,7 +35,36 @@ namespace TabularReporting.Sample.Tests
                     new OneTimeRowQuery( // 2b. Define second header row
                         new ColumnQueryWithStr("Tested by"),
                         new ColumnQueryWithStr("Me")),
-                    new ByAssertTypeFilter("Equal"), new Coun)
+                    new OneTimeRowQuery( // 2c. Define third header row
+                        new ColumnQueryWithStr("Final result"), 
+                        new ColumnQueryWithStr(result.Result.ToString())),
+                    // 2c. Define body
+                    new ByAssertTypeFilter("Equal", counterColQuery = new CounterColumnQuery(), new NameGetter(), new ExecTimeInSecondsGetter(), new ResultGetter()),
+                    new ByAssertTypeFilter("MultiTrue", counterColQuery, new NameGetter(), new ExecTimeInSecondsGetter(), new ColumnQueryWithRows(
+                        new EveryRowQuery<EnumerableTestResult>(
+                            new NameGetter(), new ExecTimeInSecondsGetter(), new ResultGetter()))));
+
+            // 3. Report
+            IColumn reportedColumn =
+                new Reporter<EnumerableTestResult>().Report(new EnumerableTestResult(result), reportQuery);
+
+            // 3a. Preview column
+            string columnStr = reportedColumn.ToXml().ToString();
+
+            // 4. Format
+            string formattedReport = new SimpleTextFormatter().Format(reportedColumn);
+
+            // 5. Write
+            string reportPath = new SimpleTextWriter().WriteReport(formattedReport, Path.GetTempPath(), "MyReport");
+
+            // 6. Read
+            string readReport = new SimpleTextReader().ReadReport(reportPath);
+            Assert.Equal(formattedReport, readReport);
+
+            // 7. Parse
+            IColumn parsedColumn = new SimpleTextParser().Parse(readReport);
+
+            // 8. Interpret
 
         }
     }
