91a9bb2945c8b6690ca51647cbd9f9adfb2ac24c
Anonymous <>
2020-03-15 20:20:12 +0100

    Overload Report()

--- samples/TabularReporting.TestStand/Reporter.cs
+++ samples/TabularReporting.TestStand/Reporter.cs
@@ -1,6 +1,7 @@
 ﻿using TabularReporting.Abstractions;
 using NationalInstruments.TestStand.Interop.API;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TabularReporting.TestStand
 {
@@ -15,5 +16,8 @@ namespace TabularReporting.TestStand
 
         public IColumn Report(PropertyObject source, IEnumerable<IRowQuery> reportRowQueries) =>
             _reporter.Report(new EnumerablePropertyObject(source), new ColumnQueryWithRows(reportRowQueries));
+
+        public IColumn Report(PropertyObject source, params IRowQuery[] reportRowQueries) =>
+            Report(source, reportRowQueries.AsEnumerable());
     }
 }
