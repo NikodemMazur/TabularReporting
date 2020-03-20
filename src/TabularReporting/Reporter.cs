fd7b177dc71670a584ba4c4aa8aaf851b570c79f
Anonymous <>
2020-03-20 21:20:27 +0100

    Fix; sources are now passed on down the queries composition

--- src/TabularReporting/Reporter.cs
+++ src/TabularReporting/Reporter.cs
@@ -12,16 +12,17 @@ namespace TabularReporting
         public IColumn Report(IEnumerable<T> sources, IEnumerable<IRowQuery> reportQueries) =>
             new Column(ProjectToRows(sources, reportQueries));
 
-        ColUnion PopulateColumn(T source, IColumnQuery colQuery)
+        ColUnion PopulateColumn(IEnumerable<T> sources, T source, IColumnQuery colQuery)
         {
             if (colQuery is ISourcedColumnQuery<T> sourcedColQuery)
                 sourcedColQuery.Source = source; // If the type implements ISourcedXXXQuery, you must specify the source first.
 
-            if (colQuery is IBranchingSourcedColumnQuery<T> branchingSourcedColQuery)
+            if (colQuery is IBranchingSourcedColumnQuery<T> branchingSourcedColQuery) // Branch the projection.
                 return colQuery.Content.Extract<ColUnion>(rq => new ColUnion.Case1(ProjectToRows(branchingSourcedColQuery.Branch, rq)),
                 obj => new ColUnion.Case2(obj));
 
-            return colQuery.Content.Extract<ColUnion>(rq => new ColUnion.Case1(ProjectToRows(Enumerable.Empty<T>(), rq)),
+            // If not branching, pass sources on.
+            return colQuery.Content.Extract<ColUnion>(rq => new ColUnion.Case1(ProjectToRows(sources, rq)),
             obj => new ColUnion.Case2(obj));
         }
 
@@ -29,30 +30,29 @@ namespace TabularReporting
         {
             var rowList = new List<IRow>();
 
-            // If the call doesn't have sourced queries or sources are empty, skip interating forcing projection.
-            if (rowQueries.OfType<ISourcedRowQuery<T>>().Count() == 0 || sources.Count() == 0)
-                rowList.AddRange(ProjectToRowsCore(default, rowQueries, false));
+            // If sources are empty, skip interating. It's a case where non-sourced queries are fed.
+            if (sources.Count() == 0)
+                rowList.AddRange(ProjectToRowsCore(sources, default, rowQueries)); // Note that no source is passed in.
+                                                                                   // Row queries shouldn't carry sourced row/col queries
+                                                                                   // in this case.
             else
                 foreach (T source in sources)
-                    rowList.AddRange(ProjectToRowsCore(source, rowQueries, true));
+                    rowList.AddRange(ProjectToRowsCore(sources, source, rowQueries));
 
             return rowList;
 
-            IEnumerable<IRow> ProjectToRowsCore(T source_, IEnumerable<IRowQuery> rowQueries_, bool cast)
+            IEnumerable<IRow> ProjectToRowsCore(IEnumerable<T> sources_, T source_, IEnumerable<IRowQuery> rowQueries_)
             {
                 var rowList_ = new List<IRow>();
                 foreach (IRowQuery rowQuery in rowQueries_)
                 {
-
-                    IRow row;
-
                     if (rowQuery is ISourcedRowQuery<T> sourcedRowQuery)
                         sourcedRowQuery.Source = source_; // If the type implements ISourcedXXXQuery, you must specify the source first.
 
                     if (rowQuery.Predicate)
                     {
                         // Compound new row recursively.
-                        row = new Row(rowQuery.ColumnQueries.Select(cq => new Column(PopulateColumn(source_, cq))).ToArray());
+                        var row = new Row(rowQuery.ColumnQueries.Select(cq => new Column(PopulateColumn(sources_, source_, cq))).ToArray());
                         // Append the row.
                         rowList_.Add(row);
                     }
